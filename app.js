0c7a9f4c6415dbfb3ecaba64e09da571ee50182a
Rahul <rahulgoswami7061@gmail.com>
2025-10-07 21:27:12 +0530

    add a clock in repo

---
+++ app.js
@@ -0,0 +1,34 @@
+const hour=document.getElementById('hour');
+const minute= document.getElementById('minute');
+const second= document.getElementById('second');
+const day= document.getElementById('day');
+
+var monthName=[
+     'January',
+     'February',
+     'March',
+     'April',
+     'May',
+     'June',
+     'July',
+     'August',
+     'September',
+     'October',
+     'November',
+     'December',
+];
+
+const clock = setInterval(function time (){
+    let today = new Date();
+    let d= today.getDay();
+    let m = today.getMonth();
+    let y = today.getFullYear();
+    let hours = today.getHours();
+    let mins = today.getMinutes();
+    let secs = today.getSeconds();
+
+    day.innerHTML=`${d} ${monthName[m]} ${y}` //it indicates the day months and years it fetches data from const clock funct like getday()...
+    hour.textContent=hours;
+    minute.innerText=mins;
+    second.innerText=secs;
+})
